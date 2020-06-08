a39f5e9e9f1cd5e7844a5e8f0d4213f6be3e543e
Vaibhav <vjainbhl@gmail.com>
2020-06-08 20:12:04 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ class App extends Component {
   {
     return(
       <div>
-      
+      { /*<div>
+          Phone Directory   
+        </div>
+        <button>Add</button>
+        <div>
+        <span>Name</span><br/>
+        <span>Phone</span>
+        </div>*/
+      }
+      <label htmlFor='Name'>Name:</label>
+        <input type='text' placeholder='type here' defaultValue='vaibhav'/>
       
       </div>
 
